522231936f617a8b60da151ea844fcdcd8b0a5ca
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-10 22:18:17 -0300

    adicionando dependencia de validacao e adicionando elas aos atributos de candidatos

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/controllers/CadidateController.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/controllers/CadidateController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.Candidate;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/cadidate")
 public class CadidateController {
     
     @PostMapping("/")
-    public void createCandidate(@RequestBody Candidate candidate) {
+    public void createCandidate(@Valid @RequestBody Candidate candidate) {
         System.out.println("Creating candidate: " + candidate.getName());
         System.out.println(candidate.getEmail());
     }
