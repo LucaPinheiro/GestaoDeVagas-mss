786af7a142e84ace975a36e7b69cbed3a0d0a3fe
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-11 12:41:50 -0300

    adicionando anotations para gerar tabelas Entity e anotations para pk

--- src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/candidate/Candidate.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/candidate/Candidate.java
@@ -1,9 +1,15 @@
 package br.com.lucapinheiro.gestao_vagas.shared.domain.entities.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 // adicionando da dependencia do jakarta.validation para usar as anotações de validação (próprio do Spring)
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -11,8 +17,11 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data; //lib para gerar os getters e setters
 
 @Data //anotação para gerar os getters e setters de TODOS os atributos da classe
+@Entity(name = "candidate") //anotação para criar a tabela no banco de dados
 public class Candidate {
     
+    @Id //PK
+    @GeneratedValue(strategy = GenerationType.UUID) //auto increment UUID
     private UUID id;
     private String name;
 
@@ -27,5 +36,8 @@ public class Candidate {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
