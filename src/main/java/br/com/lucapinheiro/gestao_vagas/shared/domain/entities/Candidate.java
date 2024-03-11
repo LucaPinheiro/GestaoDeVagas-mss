9da2979563c2500e25e63705a0ff1aaa14ee9944
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-10 22:18:47 -0300

    chamando anotation valid para a rota

--- src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/Candidate.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/Candidate.java
@@ -2,6 +2,11 @@ package br.com.lucapinheiro.gestao_vagas.shared.domain.entities;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+// adicionando da dependencia do jakarta.validation para usar as anotações de validação (próprio do Spring)
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data; //lib para gerar os getters e setters
 
 @Data //anotação para gerar os getters e setters de TODOS os atributos da classe
@@ -9,8 +14,14 @@ public class Candidate {
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não pode conter espaços em branco ou ser vazio")
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
+
+    @Length(min=10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
