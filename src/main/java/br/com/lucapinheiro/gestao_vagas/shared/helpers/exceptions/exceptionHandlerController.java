ee96e2aa4e804ffaefe23719355ac158c6a7bc24
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-10 22:46:11 -0300

    criando o tratamento para mensagens de erro

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/helpers/exceptions/exceptionHandlerController.java
@@ -0,0 +1,35 @@
+package br.com.lucapinheiro.gestao_vagas.shared.helpers.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class exceptionHandlerController {
+
+    private MessageSource messageSource;
+
+    public exceptionHandlerController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+        
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+            ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
+            dto.add(error);
+        });
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+}
