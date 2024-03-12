b4d090c9c8b72489a67be0d82f4fa81871b5a41e
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-12 16:07:00 -0300

    ajustando nomes e imports

--- src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/repositories/cadidate/CandidateRepository.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/repositories/cadidate/CandidateRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.candidate.Candidate;
 
-public interface Candidate_repository_interface extends JpaRepository<Candidate, UUID> {
+public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
     Optional <Candidate> findByUsernameOrEmail(String username, String email);
 
 }
---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/repositories/cadidate/CompanyRepository.java
@@ -0,0 +1,12 @@
+package br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.company.Company;
+
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+    Optional<Company> findByUsernameOrEmail(String username, String email);
+}
