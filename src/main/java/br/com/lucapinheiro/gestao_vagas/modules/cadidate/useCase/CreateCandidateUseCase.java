e764a56e73b90bfe1d835d94a198c361f568121a
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-12 16:07:20 -0300

    crriando candidateUseCase

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/useCase/CreateCandidateUseCase.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/useCase/CreateCandidateUseCase.java
@@ -4,21 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.candidate.Candidate;
-import br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate.Candidate_repository_interface;
+import br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate.CandidateRepository;
 import br.com.lucapinheiro.gestao_vagas.shared.helpers.exceptions.UserFoundException;
 
 @Service
 public class CreateCandidateUseCase {
 
     @Autowired
-    private Candidate_repository_interface candidate_repository_interface;
+    private CandidateRepository candidateRepository;
 
     public Candidate execute(Candidate candidate) {
-        this.candidate_repository_interface
+        this.candidateRepository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((user) -> {
-                    throw new UserFoundException(); //Se achar o user retorna o erro falando que já existe
+                    throw new UserFoundException(); // Se achar o user retorna o erro falando que já existe
                 });
-        return this.candidate_repository_interface.save(candidate); 
+        return this.candidateRepository.save(candidate);
     }
 }
