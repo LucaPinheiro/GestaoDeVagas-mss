eacba9a0a2747ccebd01fbfcccdae2c3e5b159bb
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-11 23:07:08 -0300

    criando método findByUsernameOrEmail na interface✅

--- src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/repositories/cadidate/Candidate_repository_interface.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/repositories/cadidate/Candidate_repository_interface.java
@@ -1,5 +1,6 @@
 package br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.candidate.Candidate;
 
 public interface Candidate_repository_interface extends JpaRepository<Candidate, UUID> {
+    Optional <Candidate> findByUsernameOrEmail(String username, String email);
 
 }
