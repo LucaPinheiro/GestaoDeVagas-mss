d40686664eb460e88d8097f821abd9f78873214f
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-12 23:13:43 -0300

    adicionando validacoes

--- src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/job/Job.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/domain/entities/job/Job.java
@@ -13,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "Job")
@@ -23,14 +24,16 @@ public class Job {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String description;
-    private String level;
     private String benefits;
+    
+    @NotBlank(message = "Esse campo é obrigatório")
+    private String level;
 
     @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private Company company;
 
-    @Column(name = "company_id")
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
 
     @CreationTimestamp
