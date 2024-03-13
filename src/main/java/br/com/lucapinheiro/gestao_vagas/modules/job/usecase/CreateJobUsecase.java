213cbc554b89e15ee72f50a91e218ba8c30e7d9a
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-12 23:13:32 -0300

    criando usecase de create Job

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/job/usecase/CreateJobUsecase.java
@@ -0,0 +1,18 @@
+package br.com.lucapinheiro.gestao_vagas.modules.job.usecase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.job.Job;
+import br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate.JobRepository;
+
+@Service
+public class CreateJobUsecase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public Job execute(Job job) {
+        return this.jobRepository.save(job);
+    }
+}
