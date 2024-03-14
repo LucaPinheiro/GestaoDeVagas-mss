45cca9f98ecb8ccd817f9726102b10a3a2061aac
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-14 16:28:04 -0300

    criando um dto para criar um Job✅

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/infra/dto/CreateJobDTO.java
@@ -0,0 +1,13 @@
+package br.com.lucapinheiro.gestao_vagas.shared.infra.dto;
+
+import lombok.Data;
+
+// De acordo com o projeto geral, esse DTO serve para criar um novo Job. Ele é usado no controller de Job.
+@Data
+public class CreateJobDTO {
+    
+    private String description;
+    private String benefits;
+    private String level;
+
+}
