22573cc19f6210bdf54a05637e0c7bec0df7f756
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-14 17:48:36 -0300

    adicionando criptografia para a senha do candidato

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/create_candidate/app/CreateCandidateUseCase.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/create_candidate/app/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.lucapinheiro.gestao_vagas.modules.cadidate.create_candidate.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.candidate.Candidate;
@@ -13,12 +14,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Candidate execute(Candidate candidate) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException(); // Se achar o user retorna o erro falando que já existe
                 });
+
+                var password = passwordEncoder.encode(candidate.getPassword());
+                candidate.setPassword(password);
+                
         return this.candidateRepository.save(candidate);
     }
 }
