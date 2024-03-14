cc5424bbf024661c1d6cc75c38eab845d46d2271
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-14 15:36:13 -0300

    Finalizando parte JWT

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/providers/JWTProvider.java
@@ -0,0 +1,36 @@
+package br.com.lucapinheiro.gestao_vagas.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+// Esta classe serve para validar o token JWT que é enviado nas requisições. Depois de validar o token,
+// ela retorna o id autenticado. E se o token não for válido, ela retorna uma string vazia.
+
+@Service
+public class JWTProvider {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    public String validateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        try {
+            var subject = JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+            return subject;
+        } catch (JWTVerificationException ex) {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+}
