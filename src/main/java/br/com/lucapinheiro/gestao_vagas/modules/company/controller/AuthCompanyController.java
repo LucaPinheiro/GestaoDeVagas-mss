29e12619e51976d001c8bbfdc961476f523071c8
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-14 15:10:01 -0300

    controller que recebe requisições para autenticar uma empresa

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/company/controller/AuthCompanyController.java
@@ -0,0 +1,34 @@
+package br.com.lucapinheiro.gestao_vagas.modules.company.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucapinheiro.gestao_vagas.modules.company.usecase.AuthCompanyUsecase;
+import br.com.lucapinheiro.gestao_vagas.shared.infra.dto.AuthCompanyDTO;
+
+// Está classe é um controller que recebe requisições para autenticar uma empresa e retorna um token JWT
+// para a empresa autenticada. Se a empresa não existir ou a senha estiver incorreta, retorna um erro 401. Vulgo Unauthorized.
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCompanyController {
+
+    @Autowired
+    private AuthCompanyUsecase authCompanyUsecase;
+
+    @PostMapping("/company")
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
+        try {
+            var result = this.authCompanyUsecase.execute(authCompanyDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
