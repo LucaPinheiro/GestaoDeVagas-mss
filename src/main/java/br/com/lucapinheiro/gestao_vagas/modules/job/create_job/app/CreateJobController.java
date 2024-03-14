7bdbb9a06e98004e55282ab22df6f300feb1f58a
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-14 16:27:10 -0300

    Refatorando create Job controller ✅

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/job/create_job/app/CreateJobController.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/job/create_job/app/CreateJobController.java
@@ -1,5 +1,7 @@
 package br.com.lucapinheiro.gestao_vagas.modules.job.create_job.app;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lucapinheiro.gestao_vagas.shared.domain.entities.job.Job;
+import br.com.lucapinheiro.gestao_vagas.shared.infra.dto.CreateJobDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,8 +22,19 @@ public class CreateJobController {
     private CreateJobUsecase createJobUsecase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> createJob(@Valid @RequestBody Job job) {
+    public ResponseEntity<Object> createJob(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         try {
+            var companyId = request.getAttribute("company_id");
+
+            // Isto é para garantir que o companyId é um UUID válido e não um valor aleatório
+            // que pode ser passado na requisição, e não no body da requisição.
+            var job = Job.builder()
+                    .benefits(createJobDTO.getBenefits())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .description(createJobDTO.getDescription())
+                    .level(createJobDTO.getLevel())
+                    .build();
+
             var result = this.createJobUsecase.execute(job);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
