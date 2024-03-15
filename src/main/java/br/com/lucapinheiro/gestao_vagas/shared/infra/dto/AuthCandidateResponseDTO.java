e292aee71a95f8fd2662a505b2d54c49b1b53121
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-15 19:06:25 -0300

    DTO é responsável por retornar o token de autenticação para o usuário se autenticar (Candidate)

---
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/shared/infra/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,20 @@
+package br.com.lucapinheiro.gestao_vagas.shared.infra.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Esse DTO é responsável por retornar o token de autenticação para o usuário se autenticar
+// Ele é utilizado na camada de aplicação do módulo de candidato para retornar o token de autenticação
+// Se não for possível autenticar o usuário, é lançada uma exceção de autenticação
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+
+    private String acess_token;
+
+}
