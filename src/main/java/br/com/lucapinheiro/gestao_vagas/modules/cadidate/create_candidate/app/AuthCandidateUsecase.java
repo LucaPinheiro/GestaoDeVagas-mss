f2f586df4bebf1218da85b409d79c9cc9e25eb08
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-16 15:36:21 -0300

    adicionando ao body da response o tempo de expiração do token ✅

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/create_candidate/app/AuthCandidateUsecase.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/cadidate/create_candidate/app/AuthCandidateUsecase.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -18,8 +19,8 @@ import br.com.lucapinheiro.gestao_vagas.shared.domain.repositories.cadidate.Cand
 import br.com.lucapinheiro.gestao_vagas.shared.infra.dto.AuthCandidateRequestDTO;
 import br.com.lucapinheiro.gestao_vagas.shared.infra.dto.AuthCandidateResponseDTO;
 
-
 // Classe para autenticar o usuário e gerar o token de autenticação
+@Service
 public class AuthCandidateUsecase {
 
     @Value("${security.token.secret.candidate}")
@@ -32,7 +33,8 @@ public class AuthCandidateUsecase {
     private PasswordEncoder passwordEncoder;
 
     // Esse método serve para vereificar se o usuário existe no banco de dados
-    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+            throws AuthenticationException {
         var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
                 .orElseThrow(() -> {
                     throw new UsernameNotFoundException("Username/password incorrect");
@@ -45,18 +47,20 @@ public class AuthCandidateUsecase {
         }
 
         Algorithm algorithm = Algorithm.HMAC256("secret"); // Algoritiimo de criptografia
+        var expiresIn = Instant.now().plus(Duration.ofHours(1)); // Data de expiração do token
 
         // Esse token é gerado para o usuário se autenticar
         var token = JWT.create()
                 .withIssuer("javagas")
                 .withSubject(candidate.getId().toString())
                 .withClaim("roles", Arrays.asList("candidate"))
-                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                .withExpiresAt(expiresIn)
                 .sign(algorithm);
 
         var authCandidateResponse = AuthCandidateResponseDTO.builder()
-        .acess_token(token)
-        .build();
+                .acess_token(token) // Token de autenticação
+                .expires_in(expiresIn.toEpochMilli()) // Data de expiração do token
+                .build(); 
 
         return authCandidateResponse;
     }
