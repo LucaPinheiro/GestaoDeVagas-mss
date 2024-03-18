d51c9331180cb1b9900660f6abc0f669c28a82e8
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-18 11:37:57 -0300

    adding @GetMapping("/") mas ainda erro ❌

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/profileCandidate/get_profileCandidate/app/GetProfileCandidateController.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/profileCandidate/get_profileCandidate/app/GetProfileCandidateController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class GetProfileCandidateController {
     @Autowired
     private GetProfileCandidateUsecase getProfileCandidateUsecase;
 
+    @GetMapping("/")
     public ResponseEntity<Object> getProfileCandidate(HttpServletRequest request) {
 
         var idCandidate = (String) request.getAttribute("candidate_id");
