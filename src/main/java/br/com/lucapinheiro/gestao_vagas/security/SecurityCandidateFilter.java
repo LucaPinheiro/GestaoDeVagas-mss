30f079e8128da1622f858b46547f867eafbb7c41
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-19 22:18:05 -0300

    adicionando uma validação de role✅

--- src/main/java/br/com/lucapinheiro/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,8 +1,11 @@
 package br.com.lucapinheiro.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,7 +27,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         // Zerando as autenticações
-        SecurityContextHolder.getContext().setAuthentication(null);
+        // SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if (request.getRequestURI().startsWith("/candidate")) {
@@ -37,7 +40,18 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                 }
 
                 request.setAttribute("candidate_id", token.getSubject());
-                var roles = token.getClaim("roles");
+                var roles = token.getClaim("roles").asList(Object.class);
+
+                var grants = roles.stream()
+                        .map(
+                                role -> new SimpleGrantedAuthority("ROLE_" + role.toString()))
+                        .toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        token.getSubject(), null,
+                        grants);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
         filterChain.doFilter(request, response);
