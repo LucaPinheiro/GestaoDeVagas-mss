f31ddd778463beb3945ead6f267eec1223042921
LucaPinheiro <lucapgomes11@gmail.com>
2024-03-19 22:17:42 -0300

    Apenas candidatos podem acessar essa rota (ver SecurityConfig.java)

--- src/main/java/br/com/lucapinheiro/gestao_vagas/modules/profileCandidate/get_profileCandidate/app/GetProfileCandidateController.java
+++ src/main/java/br/com/lucapinheiro/gestao_vagas/modules/profileCandidate/get_profileCandidate/app/GetProfileCandidateController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,13 @@ public class GetProfileCandidateController {
     private GetProfileCandidateUsecase getProfileCandidateUsecase;
 
     @GetMapping("/")
+    @PreAuthorize("hasRole('CANDIDATE')") // Apenas candidatos podem acessar essa rota (ver SecurityConfig.java)
     public ResponseEntity<Object> getProfileCandidate(HttpServletRequest request) {
 
         var idCandidate = (String) request.getAttribute("candidate_id");
 
         try {
-            var profile = this.getProfileCandidateUsecase.execute(UUID.fromString(idCandidate.toString()));
+            var profile = this.getProfileCandidateUsecase.execute(UUID.fromString(idCandidate.toString().toUpperCase()));
             return ResponseEntity.ok().body(profile);
 
         } catch (Exception e) {
